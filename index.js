2b5759ab2e470d64e9e063af655398e416099abc
Christopher Dennis <cada2020@mymail.pomona.edu>
2023-01-05 13:45:24 -0500

    update index.js

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ const app = express()
 app.use(cors())
 app.use(express.static(path.resolve(__dirname, '../build/')))
 
-app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, '../build/', 'index.html'));
+app.get('*', (req, res) => {
+    const index = path.resolve('../build/index.html');
+    res.sendFile(index);
   });
 
 app.get('/', (req,res) => {
